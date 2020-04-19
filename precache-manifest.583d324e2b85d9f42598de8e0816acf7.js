e7a6d3474059eee249b456917df875c071890d00
chimdia <fortunechimdiya@gmail.com>
2020-04-19 14:52:00 +0100

    Updates

---
+++ precache-manifest.583d324e2b85d9f42598de8e0816acf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5ca771950b434abd4e0b279cdc6388",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c2594979c5ddf9cb6501",
+    "url": "/monsters/static/css/main.300a09c2.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2594979c5ddf9cb6501",
+    "url": "/monsters/static/js/main.0573b1b1.chunk.js"
+  },
+  {
+    "revision": "341975cc8ff21a216c4c",
+    "url": "/monsters/static/js/runtime-main.7edf1021.js"
+  }
+]);
